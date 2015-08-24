1521c1488bfc5043926bff4ceacd85c73168b31a
Juanjo <Juanjo@192.168.1.16>
2015-08-23 21:06:37 -0300

    Puse mi nombre

--- AlvearControlEmployee/src/alvearcontrolemployee/AlvearControlEmployee.java
+++ AlvearControlEmployee/src/alvearcontrolemployee/AlvearControlEmployee.java
@@ -15,7 +15,7 @@ public class AlvearControlEmployee {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Seb");
     }
     
 }
