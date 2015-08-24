bac24e9fdfcec365e4c6934bfd56b4b20e2b3afe
juanjoalbo007 <juanjoalbo@gmail.com>
2015-08-24 19:31:00 -0300

    Seb: prueba crear news Empleados y Ocurrencias

--- AlvearControlEmployee/src/alvearcontrolemployee/AlvearControlEmployee.java
+++ AlvearControlEmployee/src/alvearcontrolemployee/AlvearControlEmployee.java
@@ -5,6 +5,8 @@
  */
 package alvearcontrolemployee;
 
+import java.util.Calendar;
+
 /**
  *
  * @author JJ
@@ -15,7 +17,24 @@ public class AlvearControlEmployee {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Seb");
+        //System.out.println("Seb");
+        Empleado e1 = new Empleado("Juan","Perez");
+        Empleado e2 = new Empleado("Maria","Juarez");
+        System.out.println(e1.getApellido()+", "+e1.getNombre());
+        System.out.println(e2.getApellido()+", "+e2.getNombre());
+        
+        Ocurrencia o1 = new Ocurrencia();
+        Ocurrencia o2 = new Ocurrencia();
+        System.out.println("Occurrencia1: "+o1.getFechaYHora());
+        System.out.println("Occurrencia2: "+o2.getFechaYHora());
+        Calendar fecha = Calendar.getInstance();
+        fecha.add(Calendar.HOUR_OF_DAY,-5);
+        o2.setFechaYHora( fecha.getTime());
+        System.out.println("Occurrencia2: "+o2.getFechaYHora());
+        System.out.println("comparación fechas1 y 2: "+o2.getFechaYHora().compareTo(o1.getFechaYHora()));
+        
+        System.out.println("Get sólo año: "+fecha.get(Calendar.YEAR));
+        
     }
     
 }
